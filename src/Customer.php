eac719b215b5cdc08e21c457e5f2ecc659e3ad3d
fan-lab <isfanrachmadavianto.if@gmail.com>
2023-10-02 09:05:01 +0700

    Untuk percobaan Remote

--- src/Customer.php
+++ src/Customer.php
@@ -11,4 +11,8 @@ class Customer{
     public function sayHello(string $name): string{
         return "Hello $name, my name is $this->$name";
     }
+
+    public function untukTesting(): string{
+        return "Ini adalah keperluan Testing";
+    }
 }
